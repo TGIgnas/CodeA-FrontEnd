0606923ec1e1f3c26e06b627cdc628d0f404022f
TGIgnas <92112858+TGIgnas@users.noreply.github.com>
2022-04-04 21:12:07 +0300

    60 paskaita

---
+++ 60 paskaita/main.js
@@ -0,0 +1,33 @@
+let x = 10 ** 2;
+
+console.log(x)
+
+let firstName = "Will";
+let lastName = "Smith";
+
+let text = `Welcome ${firstName} ${lastName}!`;
+
+console.log(text)
+
+const result = 150 / 15;
+
+console.log(result)
+
+const cars = [];
+cars[0]= "Audi";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+
+console.log(cars)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];
+console.log(fruits)
+
+console.log(2==2);
+
+console.log(false == 0);
+
+
+console.log(3 === '3');
+
